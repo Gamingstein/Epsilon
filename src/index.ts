ac938eaacfae8ddeb0023af5db851d46bc7bdfd8
Gamingstein <timeblackhole980@gmail.com>
2025-04-13 18:59:39 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import {
   GatewayIntentBits,
   MessageFlags,
   Interaction,
-  OmitPartialGroupDMChannel,
-  Message,
 } from "discord.js";
 import { token, googleAPIKey } from "./config.json";
 import { GoogleGenAI } from "@google/genai";
@@ -97,9 +95,9 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 async function getAIResponse(
   messageContent: string,
-  message: OmitPartialGroupDMChannel<Message<boolean>>,
+  username: string,
 ): Promise<string> {
-  const inputMessage = `Your name is Epsilon, an advanced and friendly AI developed by Gamingstein for Discord. ${message.author.username} has asked you: "${messageContent}". Respond in a concise, engaging, and human-like manner. Show appropriate emotions and empathy, and make minor mistakes occasionally, correcting them in future interactions. Remember the context of the ongoing conversation and respond accordingly. Use Discord-specific features like emojis, text formatting (bold, italic, monospace), and casual language. Provide only the response you would give to them, nothing else.`;
+  const inputMessage = `Your name is Epsilon, an advanced and friendly AI developed by Gamingstein for Discord. "${username}" has asked you: "${messageContent}". Respond in a concise, engaging, and human-like manner. Show appropriate emotions and empathy, and make minor mistakes occasionally, correcting them in future interactions. Remember the context of the ongoing conversation and respond accordingly. Use Discord-specific features like emojis, text formatting (bold, italic, monospace), and casual language. Provide only the response you would give to them, nothing else.`;
 
   // const inputMessage = `Your name is Epsilon, an all-in-one Smart Bot for Discord developed by Gamingstein. ${message.author.username} asked you the following: "${messageContent}". Respond briefly and naturally, as if you were a human conversing with another human. Show emotions, even if they are simulated, and make minor mistakes occasionally, but correct them in future interactions. Remember the ongoing conversation and respond accordingly. Chat as if you are on Discord, using emojis, text formatting like bold, italic, and monospace. Only provide the response you would give to them, nothing else.`;
 
@@ -123,7 +121,7 @@ client.on(Events.MessageCreate, async (message) => {
   if (!message.mentions.users.has(client.user?.id ?? "")) return;
 
   const userMessage = message.content.replace(/<@!?(\d+)>/, "").trim();
-  const botResponse = await getAIResponse(userMessage, message);
+  const botResponse = await getAIResponse(userMessage, message.author.username);
 
   message.reply(botResponse);
 });
